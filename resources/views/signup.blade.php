32d7fef1b6505602867fa832a56fa5432df3cefd
unknown <ranjan.kaundal@gmail.com>
2019-12-09 23:20:33 +0530

    datepicker added

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,6 +5,10 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+      <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
       <title>Signup</title>
       <style>
          *{ margin:0; padding:0;}
@@ -67,7 +71,7 @@
                               </div>
                               <div class="col form-group">
                                  <label>Date of Birth</label>
-                                 <input type="text" name="dob" class="form-control" placeholder=" ">
+                                 <input id="datepicker" type="text" name="dob" class="form-control" placeholder="">
                               </div>
                            </div>
                            <div class="form-group">
@@ -114,8 +118,14 @@
             </div>
          </div>
       </div>
+      <script type="text/javascript">
+         $('#datepicker').datepicker({
+            dateFormat:'yy-mm-dd'
+         });
+      </script>
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      
    </body>
 </html>
